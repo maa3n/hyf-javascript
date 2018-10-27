89bb0776b6ec7f2def3f660440cf3c477be7f2aa
Maen Al Mahameed <41008566+maa3n@users.noreply.github.com>
2018-10-27 12:14:19 +0200

    Update script.js

--- week1/script.js
+++ week1/script.js
@@ -70,36 +70,36 @@ console.log('The Value of my variable f is : ' + f);
 console.log('The type  of my variable f is : ' + typeof f);
 
 if (c===d){
-	console.log( 'The variables c and d have the same type: '  + typeof c + typeof d )
+	console.log( 'The variables c and d have the same value: '  + typeof c + typeof d )
 } else {
-	console.log( 'The variables c and d have a different type: '  + typeof c + typeof d  )
+	console.log( 'The variables c and d have a different value: '  + typeof c + typeof d  )
 }
 
 if (d===e){
-	console.log( 'The variables d and e have the same type: '  + typeof d + typeof e )
+	console.log( 'The variables d and e have the same value: '  + typeof d + typeof e )
 } else {
-	console.log( 'The variables d and e have a different type: '  + typeof d + typeof e  )
+	console.log( 'The variables d and e have a different value: '  + typeof d + typeof e  )
 }
 
 
 if (e===f){
-	console.log( 'The variables e and f have the same type: '  + typeof e + typeof f )
+	console.log( 'The variables e and f have the same value: '  + typeof e + typeof f )
 } else {
-	console.log( 'The variables e and f have a different type: '  + typeof e + typeof f  )
+	console.log( 'The variables e and f have a different value: '  + typeof e + typeof f  )
 }
 
 
 if (e===e){
-	console.log( 'The variables e and e have the same type: '  + typeof e + typeof e )
+	console.log( 'The variables e and e have the same value: '  + typeof e + typeof e )
 } else {
-	console.log( 'The variables e and e have a different type: '  + typeof e + typeof e  )
+	console.log( 'The variables e and e have a different value: '  + typeof e + typeof e  )
 }
 
 
 if (f===f){
-	console.log( 'The variables f and f have the same type: '  + typeof f + typeof f )
+	console.log( 'The variables f and f have the same value: '  + typeof f + typeof f )
 } else {
-	console.log( 'The variables f and f have a different type: '  + typeof f + typeof f  )
+	console.log( 'The variables f and f have a different value: '  + typeof f + typeof f  )
 }
 
 /*-----------9---------*/
